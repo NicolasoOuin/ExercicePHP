96d576f48b6f6bba217ceb957619c6bdf04ca78b
NicolasoOuin <nilouin@gmail.com>
2017-03-15 14:25:05 +0100

    Ajout d'un nouveau patient: Wonderwomen

--- testPersonne.php
+++ testPersonne.php
@@ -9,3 +9,9 @@ $patient = new Patient ("Leclerc","17171633623671");
 echo 'Monsieur '.$patient->getNom().' est '.$superman->getNom().' et son numéro de sécurité sociale est '.$patient->getNumsecu().'<br>';
 
 
+$wonderwomen = new Personne ("Wonderwomen");
+$patient2 = new Patient ("Loic","1937329387232");
+
+echo 'Madame '.$patient2->getNom().' est '.$wonderwomen->getNom().' et son numéro de sécu est '.$patient2->getNumsecu();
+
+
