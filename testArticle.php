1105a965d5a13e9357686901fdaf86611bd9f003
NicolasoOuin <nilouin@gmail.com>
2017-03-15 15:31:44 +0100

    :lolipop: Ecriture d'un Article... 0.3
    Connexion à la bdd et requêtes sql

--- testArticle.php
+++ testArticle.php
@@ -1,8 +1,11 @@
 ﻿<?php
 
+require_once ('Connexionbdd.php');
 require_once("Article.php");
 
-$monpremierarticle = new Article ("Titre de l'article à rechercher en SQL", "Contenu de l'article à rechercher en SQL");
+$sql=
+
+$monpremierarticle = new Article ("$donnees())", "Contenu de l'article à rechercher en SQL");
 
 echo '<h1>Titre: '.$monpremierarticle->getTitlearticle().'</h1><br>';
 echo '<p>Contenu: '.$monpremierarticle->getContenu().'</p>';
